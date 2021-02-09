795d8a75dcf7b1a34a62b5b99d47a6d33c86299d
unknown <alao@lorma.edu>
2021-02-09 14:18:33 +0800

    first commit

---
+++ src/components/Department.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <div  class="flex flex-col w-1/3">
+      <div class="flex flex-wrap bg-gray-300 p-8 justify-start">
+        <label for="department" class="w-full text-left">Department</label>
+        <input type="text" id="department" class="border-gray-900 w-full" />
+      </div>
+
+      <div class="flex justify-end p-2 bg-gray-200 ">
+        <button class="bg-indigo-900 text-white rounded-md p-2">
+          Create Department
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
